d57fa2eb5c297a83689458b3d7422abac73510b4
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-10-07 12:02:44 +0700

    Push KL_Random_02 Dice_Img_Update

--- app/src/main/java/com/example/kl_hd_02_happybirthday/KL_Random/ActivityRandom.kt
+++ app/src/main/java/com/example/kl_hd_02_happybirthday/KL_Random/ActivityRandom.kt
@@ -3,91 +3,67 @@ package com.example.kl_hd_02_happybirthday.KL_Random
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.kl_hd_02_happybirthday.R
+import kotlin.IllegalArgumentException
 
 class ActivityRandom : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
-        val rollButton: Button= findViewById(R.id.btnRoll)
+        val rollButton: Button= findViewById(R.id.btn)
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val dice1 = Dice1(6)
-        val dice2 = Dice2(6)
-        val dice3= Dice3(6)
-        val diceRoll1 = dice1.roll()
-        val diceRoll2 = dice2.roll()
-        val diceRoll3 = dice3.roll()
-        val resultTextView1: TextView = findViewById(R.id.number1)
-        val resultTextView2: TextView = findViewById(R.id.number2)
-        val resultTextView3: TextView = findViewById(R.id.number3)
-        resultTextView1.text = diceRoll1.toString()
-        resultTextView2.text = diceRoll2.toString()
-        resultTextView3.text = diceRoll3.toString()
-        val txtTong= diceRoll1+diceRoll2+diceRoll3
+        val (dice1, dice2, dice3) = List(3){Dice(6)}
+        val diceRolls = listOf(dice1.roll(), dice2.roll(), dice3.roll())
+        val diceImgs = listOf(
+            findViewById<ImageView>(R.id.imgDice1),
+            findViewById<ImageView>(R.id.imgDice2),
+            findViewById<ImageView>(R.id.imgDice3)
+        )
+        val diceText = listOf(
+            findViewById<TextView>(R.id.diemDice1),
+            findViewById<TextView>(R.id.diemDice2),
+            findViewById<TextView>(R.id.diemDice3)
+        )
+        for(i in diceRolls.indices){
+            val diceRoll= diceRolls[i]
+            val diceImg= diceImgs[i]
+            val ganAnh= when(diceRoll){
+                1-> R.drawable.dice_1
+                2-> R.drawable.dice_2
+                3-> R.drawable.dice_3
+                4-> R.drawable.dice_4
+                5-> R.drawable.dice_5
+                6-> R.drawable.dice_6
+
+                else -> throw IllegalArgumentException ("Loi")
+            }
+            diceImg.setImageResource(ganAnh)
+        }
+        diceText[0].text=diceRolls[0].toString()
+        diceText[1].text=diceRolls[1].toString()
+        diceText[2].text=diceRolls[2].toString()
+        val diceKQ: TextView= findViewById(R.id.txtDiemDice)
+        val diceTong: TextView= findViewById(R.id.txtTong)
+        val txtTong = diceRolls.sum()
         if(txtTong in 3..10){
-            val txtKq :TextView = findViewById(R.id.kq)
-            val txtDiem :TextView = findViewById(R.id.txtTong)
-            txtKq.text="Xỉu"
-            txtDiem.text= txtTong.toString()
-        }else if(txtTong in 11..18){
-            val txtKq :TextView = findViewById(R.id.kq)
-            val txtDiem :TextView = findViewById(R.id.txtTong)
-            txtKq.text="Tài"
-            txtDiem.text= (txtTong).toString()
+            diceKQ.text="Xiu"
         }
-    }
-
-    class Dice1(val numSides: Int) {
-
-        fun roll(): Int {
-            return (1..numSides).random()
+        if(txtTong in 11.. 18){
+            diceKQ.text="Tai"
         }
+        diceTong.text=(txtTong).toString()
     }
-    class Dice2(val numSides: Int) {
+    class Dice(val numberSide: Int){ fun roll()= (1..numberSide).random()}
 
-        fun roll(): Int {
-            return (1..numSides).random()
-        }
-    }
-    class Dice3(val numSides: Int) {
 
-        fun roll(): Int {
-            return (1..numSides).random()
-        }
-    }
 }
-
-//class ActivityRandom : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_random)
-//
-//        val rollButton: Button = findViewById(R.id.btnRoll)
-//        val resultTextViews = listOf<TextView>(findViewById(R.id.number1), findViewById(R.id.number2), findViewById(R.id.number3))
-//        val txtKq: TextView = findViewById(R.id.kq)
-//        val txtDiem: TextView = findViewById(R.id.txtTong)
-//
-//        rollButton.setOnClickListener {
-//            val diceRolls = List(3) { Dice(6).roll() }
-//            resultTextViews.forEachIndexed { index, textView -> textView.text = diceRolls[index].toString() }
-//
-//            val txtTong = diceRolls.sum()
-//            txtDiem.text = txtTong.toString()
-//            txtKq.text = if (txtTong in 3..10) "Xỉu" else "Tài"
-//        }
-//    }
-//
-//    class Dice(val numSides: Int) {
-//        fun roll(): Int = (1..numSides).random()
-//    }
-//}
-//Điều này rút ngắn mã bằng cách sử dụng List để lưu trữ các TextView và duyệt qua chúng, sử dụng forEachIndexed. Cũng đã kết hợp việc khai báo các biến và gán giá trị cho chúng thành một phần của việc khai báo ban đầu.
 
 
 
