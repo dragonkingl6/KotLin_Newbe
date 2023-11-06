6d06b8b6145ffc586d2bab780058e0a5e9bea338
Mai Tiến Đức -Ph20223 <124181138+dragonkingl6@users.noreply.github.com>
2023-11-06 21:52:29 +0700

    Push KL_Layout

---
+++ app/src/main/java/com/example/kl_hd_02_happybirthday/Layout/LinearLayout/ActivityLinearLayout.kt
@@ -0,0 +1,12 @@
+package com.example.kl_hd_02_happybirthday.Layout.LinearLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kl_hd_02_happybirthday.R
+
+class ActivityLinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
---
+++ app/src/main/java/com/example/kl_hd_02_happybirthday/Layout/RealativeLayout/ActivityRealativeLayout.kt
@@ -0,0 +1,12 @@
+package com.example.kl_hd_02_happybirthday.Layout.RealativeLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kl_hd_02_happybirthday.R
+
+class ActivityRealativeLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realative_layout)
+    }
+}
